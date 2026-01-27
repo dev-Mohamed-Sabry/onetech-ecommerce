8a516920b106a74f14a1134993a0609060f01a40
MS <mo7ammed.sabre@gmail.com>
2026-01-27 15:43:57 +0200

    prototype

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\WebsiteController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(WebsiteController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/blog', 'blog')->name('blog');
+});
