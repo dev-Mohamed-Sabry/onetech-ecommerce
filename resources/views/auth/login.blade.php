358abba1fdc361add78dda48c0c8ecf41fc8a45f
MS <mo7ammed.sabre@gmail.com>
2026-02-01 10:15:11 +0200

    login & register pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.dashboard.dashboard')
+
+@section('title', 'Login')
+
+@section('content')
+
+    <body>
+
+        <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-100v">
+
+            <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white">
+                <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">starlight <span
+                        class="tx-info tx-normal">admin</span></div>
+                <div class="tx-center mg-b-60">Professional Admin Template Design</div>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Enter your username">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Enter your password">
+                    <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
+                </div><!-- form-group -->
+                <button type="submit" class="btn btn-info btn-block">Sign In</button>
+
+                <div class="mg-t-60 tx-center">Not yet a member? <a href="{{ route('register') }}" class="tx-info">Sign
+                        Up</a>
+                </div>
+            </div><!-- login-wrapper -->
+        </div><!-- d-flex -->
+
+        <script src="{{ asset('assets/backend/lib/jquery/jquery.js') }}"></script>
+        <script src="{{ asset('assets/backend/lib/popper.js/popper.js') }}"></script>
+        <script src="{{ asset('assets/backend/lib/bootstrap/bootstrap.js') }}"></script>
+
+    </body>
+
+@endsection
